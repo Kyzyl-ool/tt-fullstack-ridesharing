2685cb7ca64453dcb12e04e7d6ea515079c0535b
Nikita Izrailev <izrailev@phystech.edu>
2019-09-29 15:23:20 +0300

    added working typescript linter

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,30 @@
+module.exports =  {
+    parser:  '@typescript-eslint/parser',  // Specifies the ESLint parser
+    extends:  [
+      "eslint:recommended",
+      'plugin:react/recommended',  // Uses the recommended rules from @eslint-plugin-react
+      'plugin:@typescript-eslint/recommended',  // Uses the recommended rules from @typescript-eslint/eslint-plugin
+      'prettier/@typescript-eslint',
+      'plugin:prettier/recommended'    ],
+    plugins: ["react", "@typescript-eslint", "prettier"],
+    parserOptions:  {
+    ecmaVersion:  2018,  // Allows for the parsing of modern ECMAScript features
+    sourceType:  'module',  // Allows for the use of imports
+    ecmaFeatures:  {
+      jsx:  true,  // Allows for the parsing of JSX
+    },
+    },
+    rules: {
+      "prettier/prettier": ["error", { "singleQuote": true }],
+    },
+    env: {
+      browser: true,
+      jasmine: true,
+      jest: true
+    },
+    settings:  {
+      react:  {
+        version:  'detect',  // Tells eslint-plugin-react to automatically detect the version of React to use
+      },
+    },
+  };
