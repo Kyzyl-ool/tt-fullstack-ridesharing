789a71de37ed99efa89b34367b57290c57566ac2
Kyzyl-ool <kyzyloolk@mail.ru>
2019-10-05 05:34:40 +0300

    Colors

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,8 @@ import ChatPage from "./lib/pages/chat-page";
 import {messagesMockData} from "./mocks/messages";
 import { ThemeProvider } from '@material-ui/styles';
 import {MainTheme} from "./lib/themes/MainTheme";
+import StartingPage from "./lib/pages/starting-page";
+import AuthPage from "./lib/pages/auth-page";
 
 const App: React.FC = () => (
   <div className="App">
