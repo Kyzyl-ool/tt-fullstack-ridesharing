3f7780d8979bd41fb61c9ef7fdf2c894e5c71121
Kyzyl-ool <kyzyloolk@mail.ru>
2019-10-17 04:18:23 +0300

    yarn.lock change

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,34 +5,27 @@ import {ThemeProvider} from '@material-ui/styles';
 import {MainTheme} from './lib/themes/MainTheme';
 import StartingPage from './lib/pages/starting-page';
 import AuthPage from './lib/pages/auth-page';
-import SelectAddressPage from './lib/pages/SelectAddressPage';
-import './App.css';
 import MapComponent from './lib/components/Map';
-import {MainContainer} from './lib/containers/MainContainer';
-import {Input} from "@material-ui/core";
 import {CreateTripPage} from "./lib/pages/create-trip";
 import {SearchTripPage} from "./lib/pages/search-trip";
 
 const App: React.FC = () => (
   <div className="App">
     <ThemeProvider theme={MainTheme}>
-
-        <Router>
-          <Switch>
-            <Route exact path="/" component={StartingPage}/>
-            <Route exact path="/auth" component={AuthPage}/>
-            <Route exact path="/map" component={MapComponent}/>
-            <Route exact path="/new_trip" component={CreateTripPage}/>
-            <Route exact path="/search_trip" component={SearchTripPage}/>
-            <Redirect to="/"/>
-          </Switch>
-        </Router>
-
+      <Router>
+        <Switch>
+          <Route exact path="/" component={StartingPage}/>
+          <Route exact path="/auth" component={AuthPage}/>
+          <Route exact path="/map" component={MapComponent}/>
+          <Route exact path="/new_trip" component={CreateTripPage}/>
+          <Route exact path="/search_trip" component={SearchTripPage}/>
+          <Redirect to="/"/>
+        </Switch>
+      </Router>
 
       {/*<StartingPage />*/}
       {/*<AuthPage />*/}
       {/*<ChatPage {...messagesMockData} />*/}
-
     </ThemeProvider>
   </div>
 );
