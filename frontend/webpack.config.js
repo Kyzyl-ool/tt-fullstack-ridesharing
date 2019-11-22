3195bdc3528cfde5716d4a94a87ed398c7ada04f
Nikita Izrailev <izrailev@phystech.edu>
2019-11-22 05:38:34 +0300

    removed comments

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -22,13 +22,11 @@ module.exports = {
     authPage: './src/lib/pages/auth-page/index.tsx',
     registrationPage: './src/lib/pages/registration-page/index.tsx',
     createTripPage: './src/lib/pages/create-trip/index.tsx',
-    searchTrip: './src/lib/pages/search-trip/index.tsx',
-    selectAddressPage: './src/lib/pages/SelectAddressPage/index.tsx'
+    searchTrip: './src/lib/pages/search-trip/index.tsx'
   },
   output: {
     path: __dirname + '/dist',
     filename: '[name].[hash].js'
-    // chunkFilename: '[name].bundle.js'
   },
   devServer: {
     historyApiFallback: true,
@@ -86,11 +84,7 @@ module.exports = {
         vendor: {
           test: /[\\/]node_modules[\\/]/,
           name(module) {
-            // get the name. E.g. node_modules/packageName/not/this/part.js
-            // or node_modules/packageName
             const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
-
-            // npm package names are URL-safe, but some servers don't like @ symbols
             return `npm.${packageName.replace('@', '')}`;
           }
         }
