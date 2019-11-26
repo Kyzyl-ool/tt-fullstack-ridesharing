99479ff60fa386f343566b81dc76da2079f9e839
Kyzyl-ool <kyzyloolk@mail.ru>
2019-11-26 03:59:13 +0300

    animations

--- frontend/src/lib/components/message/index.tsx
+++ frontend/src/lib/components/message/index.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Box, Typography } from '@material-ui/core';
 import { Avatar } from '../Avatar/Avatar';
 import './Message.scss';
+import { Flag } from '../../pages/chat-page';
 
 interface IMessageProps {
   first: boolean;
@@ -17,9 +18,30 @@ interface IMessageProps {
 }
 
 const Message: React.FC<IMessageProps> = props => {
+  const [renderNumber, setRenderNumber] = useState(0);
+  const flag = useContext(Flag);
+  useEffect(() => {
+    console.log(`${flag}, from: ${props.body}`);
+    setRenderNumber(renderNumber + 1);
+  }, [flag]);
+
+  // setRenderNumber(renderNumber+1);
+  // if (!props.last) {
+  //   setFlag(false)
+  // }
+  // if (!flag && !props.last) {
+  //   setFlag(true)
+  // }
+
   return (
-    <Box display={'flex'} flexDirection={`${props.mine ? 'row-reverse' : 'row'}`} alignItems={'flex-end'} mt={0.2}>
-      <Box style={{ margin: '0px 8px 0px 8px', opacity: `${props.last ? 1 : 0}` }}>
+    <Box
+      className={['go-up-animation', 'go-up-animation_1'][renderNumber % 2]}
+      display={'flex'}
+      flexDirection={`${props.mine ? 'row-reverse' : 'row'}`}
+      alignItems={'flex-end'}
+      mt={0.2}
+    >
+      <Box className={'avatar-box'} style={{ opacity: `${props.last ? 1 : 0}` }}>
         <Avatar
           src={!props.mine ? 'https://ridesharing-bucket1.s3.amazonaws.com/31f86839cecd6ea2f661550da2aaa52a' : null}
         />
@@ -27,7 +49,9 @@ const Message: React.FC<IMessageProps> = props => {
       <div
         className={`message ${props.first ? 'message__first' : 'message__notfirst'} ${
           props.last ? 'message__last' : 'message__notlast'
-        } ${props.mine ? 'message__mine' : 'message__notmine'}
+        } ${props.last ? (!props.mine ? 'appear-left-animation' : 'appear-right-animation') : ''} ${
+          props.mine ? 'message__mine' : 'message__notmine'
+        }
         ${props.body === 'Пароль скрыт' ? 'message__password' : null}
         `}
       >
