4f487bf8b3c41036ec8b9adf3676ff7ded3e7efa
Nikita Izrailev <izrailev@phystech.edu>
2020-03-08 05:22:08 +0300

    added initial comps

---
+++ lib/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+// import * as serviceWorker from './serviceWorker';
+
+const composeEnhancers = (window['__REDUX_DEVTOOLS_EXTENSION_COMPOSE__'] as typeof compose) || compose;
+
+const store = createStore(() => {}, composeEnhancers(applyMiddleware(thunk)));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
