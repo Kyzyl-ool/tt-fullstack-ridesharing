8952574b5966fc7ed546faea94defd2ad97620a1
Kyzyl-ool Kezhik <kyzyloolk@mail.ru>
2020-03-11 02:50:51 +0300

    1

--- lib/pages/trip-creation/5/TripInfo.tsx
+++ lib/pages/trip-creation/5/TripInfo.tsx
@@ -1,12 +1,24 @@
-import React from 'react';
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+import React, { useState } from 'react';
 import { BaseLayer } from '../../../components/BaseLayer/BaseLayer';
 import { Input } from '../../../components/Input/Input';
+import { Button } from '../../../components/Button';
 
 export const TripInfo: React.FC = () => {
+  const [text, setText] = useState<string>('Создать поездку');
+  const [btnDisabled, setBtnDisabled] = useState<boolean>(false);
+  const handleClick = () => {
+    setText('Создание поездки...');
+    setBtnDisabled(true);
+  };
+
   return (
     <BaseLayer type={'secondary'} header={'Введите информацию о поездке'}>
       <Input placeholderType={'subscript'} id={'asd'} form={'asdasd'} placeholderText={'Кол-во посадочных мест'} />
       <Input placeholderType={'subscript'} id={'asd'} form={'asdasd'} placeholderText={'Стоимость поездки'} />
+      <Button disabled={btnDisabled} onClick={handleClick}>
+        {text}
+      </Button>
     </BaseLayer>
   );
 };
---
+++ lib/pages/trip-creation/index.tsx
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+
+const pages = [];
+
+export const TripCreation: React.FC = props => {
+  const [pageNumber, setPageNumber] = useState<number>(0);
+
+  return <></>;
+};
