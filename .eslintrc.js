678134aae8da946dd468daec6642c817876b78cd
Nikita Izrailev <izrailev@phystech.edu>
2020-03-11 00:48:51 +0300

    fixed lint error

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,36 +1,39 @@
-module.exports =  {
-    parser:  '@typescript-eslint/parser',
-    extends:  [
-      "eslint:recommended",
-      'plugin:react/recommended',
-      'plugin:@typescript-eslint/recommended',
-      'prettier/@typescript-eslint',
-      'plugin:prettier/recommended'    ],
-    plugins: ["react", "@typescript-eslint", "prettier"],
-    parserOptions:  {
-    ecmaVersion:  2018,
-    sourceType:  'module',
-    ecmaFeatures:  {
-      jsx:  true,
-    },
-    },
-    rules: {
-      "prettier/prettier": ["error", { "singleQuote": true }],
-      "@typescript-eslint/explicit-function-return-type": "off",
-      "@typescript-eslint/no-var-requires": "off",
-      "@typescript-eslint/interface-name-prefix": [1, { "prefixWithI": "always", "allowUnderscorePrefix": true }],
-      "react/prop-types": "off",
-      "react/no-unescape-entities": "off"
-    },
-    env: {
-      browser: true,
-      node: true,
-      jasmine: true,
-      jest: true
-    },
-    settings:  {
-      react:  {
-        version:  'detect',
-      },
-    },
-  };
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended'
+  ],
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  parserOptions: {
+    // project: 'tsconfig.json',
+    // tsconfigRootDir: '.',
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  rules: {
+    'prettier/prettier': ['error', { singleQuote: true }],
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/interface-name-prefix': [1, { prefixWithI: 'always', allowUnderscorePrefix: true }],
+    'react/prop-types': 'off',
+    'react/no-unescape-entities': 'off'
+  },
+  env: {
+    browser: true,
+    node: true,
+    jasmine: true,
+    jest: true
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  }
+};
