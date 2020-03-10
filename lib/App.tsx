4ca67e5e3219c76141d49a13fbc7a463f1e2259d
Kyzyl-ool Kezhik <kyzyloolk@mail.ru>
2020-03-11 02:51:02 +0300

    Update App.tsx

--- lib/App.tsx
+++ lib/App.tsx
@@ -1,21 +1,13 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 import React from 'react';
-import { Button } from './Components/Button';
 import { TripInfo } from './pages/trip-creation/5/TripInfo';
+import Cars from './pages/trip-creation/4/Cars';
 
 const App = () => {
   return (
     <div>
-      <Button filled={true} onClick={() => {}}>
-        Отменить поиск
-      </Button>
-      <Button disabled={true} onClick={() => {}}>
-        Применить промокод
-      </Button>
-      <Button onClick={() => {}}>Применить промокод</Button>
-      <Button filled={true} disabled={true} onClick={() => {}}>
-        Отправка запроса...
-      </Button>
+      <Cars />
+      <TripInfo />
     </div>
   );
 };
