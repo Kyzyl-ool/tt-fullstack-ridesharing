19b3b2630acc0bdc4c3a786e8b910178e4517044
Kyzyl-ool Kezhik <kyzyloolk@mail.ru>
2020-03-11 06:11:05 +0300

    avatar, mark, drivercard

--- lib/App.tsx
+++ lib/App.tsx
@@ -1,12 +1,27 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 import React from 'react';
 import './App.global.scss';
-import { TripCreation } from './pages/trip-creation';
+import { Avatar } from './components/Avatar/Avatar';
+import { DriverCard } from './components/DriverCard/DriverCard';
+
+const sampleURL = 'https://pickaface.net/gallery/avatar/unr_sample_161118_2054_ynlrg.png';
 
 const App = () => {
   return (
     <div>
-      <TripCreation />
+      <DriverCard
+        mark={10}
+        avatarSrc={sampleURL}
+        car={'Audi Q8'}
+        cost={1000}
+        destination={'Дикси продуктовый магазин'}
+        firstName={'Алексей'}
+        secondName={'Кожарин'}
+        time={'17:00'}
+        tripId={1}
+        vacations={3}
+        waiting
+      />
     </div>
   );
 };
---
+++ lib/components/Avatar/Avatar.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import './Avatar.scss';
+import { Mark, markToText } from '../Mark/Mark';
+
+type AvatarSize = 'small' | 'medium' | 'large';
+
+interface IAvatar {
+  src: string;
+  mark?: number;
+  size: AvatarSize;
+}
+
+export const Avatar: React.FC<IAvatar> = ({ src, mark, size }) => {
+  const img = (
+    <img
+      src={src}
+      alt={'Avatar'}
+      className={`avatar avatar_${size} ${mark ? `avatar_stroke avatar_stroke-${markToText(mark)}` : ''}`}
+    />
+  );
+  if (mark)
+    return (
+      <span className={'avatar-container'}>
+        {img}
+        <Mark mark={mark} />
+      </span>
+    );
+  else return img;
+};
