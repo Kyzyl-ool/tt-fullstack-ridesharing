c20616b73303f4ae8c0e8e6a2cd6eb18b8652e8b
Kyzyl-ool Kezhik <kyzyloolk@mail.ru>
2020-03-11 06:16:44 +0300

    for present

--- lib/App.tsx
+++ lib/App.tsx
@@ -3,25 +3,15 @@ import React from 'react';
 import './App.global.scss';
 import { Avatar } from './components/Avatar/Avatar';
 import { DriverCard } from './components/DriverCard/DriverCard';
+import { Button } from './components/Button';
+import { TripCreation } from './pages/trip-creation';
 
 const sampleURL = 'https://pickaface.net/gallery/avatar/unr_sample_161118_2054_ynlrg.png';
 
 const App = () => {
   return (
     <div>
-      <DriverCard
-        mark={10}
-        avatarSrc={sampleURL}
-        car={'Audi Q8'}
-        cost={1000}
-        destination={'Дикси продуктовый магазин'}
-        firstName={'Алексей'}
-        secondName={'Кожарин'}
-        time={'17:00'}
-        tripId={1}
-        vacations={3}
-        waiting
-      />
+      <TripCreation />
     </div>
   );
 };
