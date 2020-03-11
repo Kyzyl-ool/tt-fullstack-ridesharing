60e0e54e2e49b8e0c5d67a8662d5efe851e5d00b
Nikita Izrailev <izrailev@phystech.edu>
2020-03-11 05:30:14 +0300

    OrganizationList almost ready

--- lib/pages/CreateRidePage/index.tsx
+++ lib/pages/CreateRidePage/index.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 /* eslint-disable @typescript-eslint/no-empty-interface */
-import React, { PureComponent } from 'react';
+import React, { useState } from 'react';
 import { Header } from '../../components/Header';
 import { Backdrop } from '../../components/Backdrop';
 import { Input } from '../../components/Input';
@@ -8,26 +8,27 @@ import { NearestOrganizationLabel } from '../../components/NearestOrganizationLa
 import './CreateRidePage.scss';
 
 interface ICreateRidePageProps {}
-interface ICreateRidePageState {}
 
-export class CreateRidePage extends PureComponent<ICreateRidePageProps, ICreateRidePageState> {
-  render() {
-    return (
-      <div>
-        <Header iconType="menu" onIconClick={() => {}}>
-          <div className="create-ride-page__organization-label">
-            <NearestOrganizationLabel nearestOrganizationName="Mail.ru Corp." onClick={() => {}} />
-          </div>
-        </Header>
-        <Backdrop>
-          <Input
-            id="departure"
-            className="create-ride-page__input"
-            placeholderText="Откуда едете?"
-            icon={<div className="create-ride-page__input-icon" />}
-          />
-        </Backdrop>
-      </div>
-    );
-  }
-}
+export const CreateRidePage = () => {
+  const [isOrganizationListShown, setIsOrganizationListShown] = useState(false);
+
+  const onLabelClick = () => setIsOrganizationListShown(!isOrganizationListShown);
+
+  return (
+    <div>
+      <Header iconType="menu" onIconClick={() => {}}>
+        <div className="create-ride-page__organization-label">
+          <NearestOrganizationLabel nearestOrganizationName="Mail.ru Corp." onClick={onLabelClick} />
+        </div>
+      </Header>
+      <Backdrop>
+        <Input
+          id="departure"
+          className="create-ride-page__input"
+          placeholderText="Откуда едете?"
+          icon={<div className="create-ride-page__input-icon" />}
+        />
+      </Backdrop>
+    </div>
+  );
+};
