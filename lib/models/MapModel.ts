36ee9ad85589bc445ebbdecf7d2d51ef0652c4e0
Nikita Izrailev <izrailev@phystech.edu>
2020-03-25 00:14:33 +0300

    added geocoding requests

--- lib/models/MapModel.ts
+++ lib/models/MapModel.ts
@@ -2,6 +2,16 @@ import axios from 'axios';
 
 const BACKEND_STAGING_URL = 'http://tt-ridesharing-backend-staging.herokuapp.com';
 
+interface ICoordinates {
+  latitude: number;
+  longitude: number;
+}
+
+interface IGeocodingResponse {
+  address: string;
+  gps: ICoordinates;
+}
+
 export default class MapModel {
   static getNearestOrganization = async ({ latitude, longitude }) => {
     const res = await axios.get(`/api/nearest_organizations`, {
@@ -9,6 +19,20 @@ export default class MapModel {
       withCredentials: true
     });
     // console.log(res.data);
+    return res.data;
+  };
+
+  static forwardGeocoding = async (address: string) => {
+    const res = await axios.post<IGeocodingResponse>('/api/encode_address', {
+      params: { address }
+    });
+    return res.data;
+  };
+
+  static reverseGeocoding = async ({ latitude, longitude }: ICoordinates) => {
+    const res = await axios.post<IGeocodingResponse>('/api/encode_address', {
+      params: { latitude, longitude }
+    });
     return res.data;
   };
 }
