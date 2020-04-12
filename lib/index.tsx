0c1373008d7e0950e98766305f9bff2600c48d75
Kyzyl-ool Kezhik <kyzyloolk@mail.ru>
2020-04-12 15:29:30 +0300

    revert loader

--- lib/index.tsx
+++ lib/index.tsx
@@ -4,8 +4,7 @@ import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
 import { rootReducer } from 'store/reducers';
-import { Container, Loader, Segment, Grid } from 'semantic-ui-react';
-const App = React.lazy(() => import('./App'));
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 const composeEnhancers = (window['__REDUX_DEVTOOLS_EXTENSION_COMPOSE__'] as typeof compose) || compose;
@@ -15,37 +14,7 @@ const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
 
 ReactDOM.render(
   <Provider store={store}>
-    <Suspense
-      fallback={
-        <Grid container relaxed={'very'}>
-          <Grid.Column>
-            <Grid.Row color={'grey'}>&nbsp;</Grid.Row>
-            <Grid.Row color={'grey'}>&nbsp;</Grid.Row>
-            <Grid.Row color={'grey'}>&nbsp;</Grid.Row>
-            <Grid.Row color={'grey'}>&nbsp;</Grid.Row>
-            <Grid.Row color={'grey'}>&nbsp;</Grid.Row>
-            <Grid.Row color={'grey'}>&nbsp;</Grid.Row>
-            <Grid.Row color={'grey'}>&nbsp;</Grid.Row>
-            <Grid.Row color={'grey'}>&nbsp;</Grid.Row>
-            <Grid.Row color={'grey'}>&nbsp;</Grid.Row>
-            <Grid.Row color={'grey'}>&nbsp;</Grid.Row>
-            <Grid.Row color={'grey'}>&nbsp;</Grid.Row>
-            <Grid.Row color={'grey'}>&nbsp;</Grid.Row>
-            <Grid.Row color={'grey'}>&nbsp;</Grid.Row>
-            <Grid.Row>
-              <Segment padded compact raised secondary attached={'bottom'}>
-                <Loader size={'medium'} inline={'centered'} active as={'div'}>
-                  Подождите, приложение загружается...
-                </Loader>
-              </Segment>
-            </Grid.Row>
-            <Grid.Row color={'grey'}>&nbsp;</Grid.Row>
-          </Grid.Column>
-        </Grid>
-      }
-    >
-      <App />
-    </Suspense>
+    <App />
   </Provider>,
   document.getElementById('root')
 );
