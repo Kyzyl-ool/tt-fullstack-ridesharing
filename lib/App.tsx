db472cb499cc573f046d626384b92381c74302f6
Kyzyl-ool Kezhik <kyzyloolk@mail.ru>
2020-04-14 12:35:20 +0300

    Update App.tsx

--- lib/App.tsx
+++ lib/App.tsx
@@ -1,11 +1,9 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 import React from 'react';
-import { useDispatch } from 'react-redux';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import { CreateRidePage } from 'pages/CreateRidePage';
 import { JoinRidePage } from 'pages/JoinRidePage';
 import { MainPage } from 'pages/MainPage';
-import UserModel from 'models/UserModel';
 import { CreateOrganizationPage } from 'pages/CreateOrganizationPage';
 import { JoinOrganizationPage } from 'pages/JoinOrganizationPage';
 import { OrganizationPage } from 'pages/OrganizationPage';
@@ -15,7 +13,6 @@ import { Welcome } from 'pages/Welcome';
 import { RidesHistoryPage } from 'pages/RidesHistory';
 import { ProfilePage } from 'pages/ProfilePage';
 import { Backdrop } from 'components/Backdrop';
-import { setUserAction } from 'store/actions/userActions';
 import './App.global.scss';
 import { UserPage } from 'pages/UserPage';
 import { PrivateRoute } from 'components/PrivateRoute';
